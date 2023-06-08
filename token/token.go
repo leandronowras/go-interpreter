4bcdae3429964a820275b666183def640605a56c
User <user@MacBook-Pro-de-User.local>
2023-06-08 15:19:52 -0300

    define tokens

---
+++ token/token.go
@@ -0,0 +1,26 @@
+package tokens
+
+type TokenType string
+
+type Token struct {
+	Type    TokenType
+	Literal string
+}
+
+const (
+	ILLEGAL   = "ILLEGAL"
+	EOF       = "EOF"
+	IDENT     = "IDENT"
+	INT       = "INT"
+	ASSIGN    = "="
+	PLUS      = "+"
+	COMMMA    = ","
+	SEMICOLON = ";"
+	LPAREN    = "("
+	RPAREN    = ")"
+	LBRACE    = "{"
+	RBRACE    = "}"
+
+	FUNCTION = "FUNCTION"
+	LET      = "LET"
+)
